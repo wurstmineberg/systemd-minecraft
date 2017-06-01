995886c6c735da15d318c35faf4364406539bc40
Fenhl <fenhl@fenhl.net>
2017-06-01 18:26:14 +0000

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name='systemd-minecraft',
+    description='A systemd service file for one or more vanilla Minecraft servers',
+    author='Wurstmineberg',
+    author_email='mail@wurstmineberg.de',
+    packages=['minecraft']
+)
